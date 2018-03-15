c28d4357149e93302561fba315ff99e9dd0f8860
Daniel Su <36752943+DanGitSu@users.noreply.github.com>
2018-03-15 12:26:21 +1100

    Add files via upload

--- Game.py
+++ Game.py
@@ -1,13 +1,37 @@
-class Player(object):
-    def __init__(self, name, alignment='neutral', health=100):
-        self.name = name
-        self.health = health
-        self.alignment = alignment
+import time
 
-    def hurt(self, health):
-        if health <= 0
-            return RuntimeError('Death')
-        else:
-            return health
+class Player:
+    health = 100
+    name = ''
+    alignment = ''
+    def name_Player(self):
+        self.name = input('Enter your name? ')
 
-    def heal(self, health):
+def gamestart():
+    # Creates The Player
+    player1 = Player()
+    print('')
+    print('A blinding white light fills your vision as the trials begin. ')
+    print('Your vision takes a while to adjust but the familiar sterile smell')
+    print('of a dentist fills your nose. ')
+    print('')
+    input('Press enter to continue... ')
+    print('')
+    print('You find yourself in a cuboid room with nothing but the cold')
+    print('hard steel chair that you are sat on and the similarly made table')
+    print('in front of you. ')
+    print('')
+    input('Press enter to continue... ')
+    print('')
+    print('A piece of paper with a wall of nonsensical text is written on it')
+    print('A small area titled "Name?" ')
+    print('')
+    player1.name_Player()
+    print('')
+
+
+gamestart()
+# my_function
+# myVar
+# MYCOMST
+

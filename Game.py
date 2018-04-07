e535d2cb00078c6132997e5d1debad47e8789648
Daniel Su <36752943+DanGitSu@users.noreply.github.com>
2018-04-07 18:04:25 +1000

    Add files via upload

--- Game.py
+++ Game.py
@@ -1,4 +1,6 @@
 
+
+# This is the player class, The player will have a health, a name and also an inventory.
 class Player:
 
     def __init__(self):
@@ -6,19 +8,18 @@ class Player:
         self.name = ''
         self.inventory = []
 
-    def name_player(self):
-        self.name = input('Enter your name? ')
+    def name_player(self):  # This function names the player
+        self.name = input('\nEnter your name? ')
 
-    def add_inventory(self, item):
+    def add_inventory(self, item):  # This function adds takes in an item and will add it to inventory
         self.inventory.append(item)
 
-    def check_inventory(self):
+    def check_inventory(self):  # This function gives the user and interface to interact with their inventory
         print(self.name + "'s Inventory")
         print(self.inventory)
-        print('')
-        print('1) Drop an Item')
+        print('\n1) Drop an Item')
         print('2) Use an Item')
-        print('3) Close Inventory')
+        print('3) Close Inventory\n')
         choice = input('')
         print('')
         if choice == '1':
@@ -26,7 +27,7 @@ class Player:
         elif choice == '2':
             print("There's nothing to use! ")
 
-    def drop_item(self):
+    def drop_item(self):  # This function removes items from the player's inventory and adds them to room's inventory
         if len(self.inventory) >= 1:
             counter = 0
             print('What Item do you want to drop?')
@@ -35,39 +36,42 @@ class Player:
                 counter += 1
                 counter = str(counter)
                 print(counter + ') ' + l)
+            print('')
             userChoice = int(input(''))
             if 0 < userChoice <= len(self.inventory):   # all '-1''s in the if statement are for indexing
                 room.add_dropped(self.inventory[userChoice - 1])
+                print('')
                 print(self.inventory[userChoice - 1] + ' has been dropped.')
                 del self.inventory[userChoice - 1]
             else:
-                print("There's nothing there")
+                print("\nThere's nothing there")
         else:
             print('There is nothing to drop!')
 
 
+# This is the Table class, it has an inventory and also the necessary functions to interact with it.
 class Table:
     items = ["Brass Key", "Red Potion"]
 
-    def check_table(self):
+    def check_table(self):  # This function returns a value depending if there is anything on the table.
         if len(self.items) > 0:
             return 1
         else:
             return 0
 
-    def take_key(self):
+    def take_key(self): # This removes the Brass Key from the Table's inventory
         keyIndex = self.items.index('Brass Key')
         del self.items[keyIndex]
 
-    def take_pot(self):
+    def take_pot(self):  # This removes the Red Potion for the Table's inventory
         potIndex = self.items.index("Red Potion")
         del self.items[potIndex]
 
-    def show_table(self):
-        print('You approach the metal table, it is awkwardly placed in the room and does not match')
+    def show_table(self):   # This function shows what is on the table
+        print('\nYou approach the metal table, it is awkwardly placed in the room and does not match')
         print('the style and decor of the room.')
         print('')
-        if len(self.items) == 1:
+        if len(self.items) == 1:    # This test for number of items on table is no longer needed as feature was removed
             print('There is a ' + self.items[0] + ' on the table.')
         elif len(self.items) == 2:
             print('There is a ' + self.items[0] + ' and a ' + self.items[1] + ' on the table.')
@@ -86,11 +90,6 @@ class Room:
     def picked_up(self, item):
         index = self.dropped.index(item)
         del self.dropped[index]
-
-    def picked_up_multiple(self, item):
-        for eachitem in item:  #BROKEN BROKEN BROKEN
-            index = self.dropped.index(eachitem)
-            del self.dropped[index]
 
     def room_check(self):
         if len(self.dropped) > 0:
@@ -117,9 +116,7 @@ def title_art():
 def game_start():
     # Creates The Player
     title_art()
-    input('')
-    print('')
-    print('A blinding white light fills your vision as the trials begin. ')
+    print('\n\nA blinding white light fills your vision as the trials begin. ')
     print('Your vision takes a while to adjust but the familiar sterile smell')
     print('of a dentist fills your nose. ')
     print('')
@@ -128,63 +125,49 @@ def game_start():
     print('in front of you. ')
     print('')
     input('Press enter to continue... ')
-    print('')
-    print('')
-    print('A piece of paper with a wall of nonsensical text is written on it')
+    print('\nA piece of paper with a wall of nonsensical text is written on it')
     print('A small area titled "Name?" ')
-    print('')
-# Names the player
-    player1.name_player()
-    print('')
-    print('')
+    player1.name_player()   # Names the player
 
 
 # The First room First Choice.
 def room_1():
-    print('The paper disappears from the table and so does the light.')
-    print('')
+    print('\nThe paper disappears from the table and so does the light.')
     print("An uncomfortable amount of time passes and suddenly everything appears.")
     print("Your now sat on a wooden chair in a new room. The room looks like it's")
     print('part of a wooden lodge. There is a table with various items on it and a')
     print('wooden door with a lock.')
-    print('')
 
     loop = 1
-    firsttime = 1
+    firsttime = 1   # firstTime is to detect if the dagger on the wall
     while loop == 1:
-        userChoice = 0
-        print('')
-        print('What will you do? ')
+        print('\nWhat will you do? ')
         print('1) Look Around')
         print('2) Check Inventory')
         print('3) Go to Table')
         print('4) Open the Door')
-        print('')
-        userChoice = input("")
+        userChoice = input("\n")
 # Dagger pickup prompt
-        if userChoice == '1':
-            if firsttime == 1:
-                print('You look around the various cabinets, dressers and crates around the room,')
+        if userChoice == '1':   # This is the 'Look Around' choice for room_1
+            if firsttime == 1:  # firstTime is to detect if the dagger on the wall
+                print('\nYou look around the various cabinets, dressers and crates around the room,')
                 print('you do not find anything of interest except a Silver Orcish Dagger hanging ')
                 print('from the wall.')
-                x = input('Do you take it? \n 1) Yes \n 2) No\n')
+                x = input('Do you take it? \n 1) Yes \n 2) No\n\n')
                 if x == '1':
                     player1.add_inventory("Orcish Dagger")
-                    print('Item Added, Orcish Dagger')
+                    print('\nItem Added, Orcish Dagger')
                     firsttime = 0
                 elif x == '2':
-                    print('You leave the dagger on the wall.')
-                    print('')
+                    print('\nYou leave the dagger on the wall.')
             else:
                 if room.room_check() == 0:
-                    print('')
-                    print("The room looks like it's part of a wooden lodge. There is a table with various items on it ")
-                    print("and a wooden door with a lock.")
-                    print('')
+                    print("\nThe room looks like it's part of a wooden lodge. There is a table with various items")
+                    print(" on it and a wooden door with a lock.")
                 else:
                     if len(room.dropped) == 1:
-                        print('You trip over an item that you previously dropped.')
-                        print('1)' + room.dropped[0])
+                        print('\nYou trip over an item that you previously dropped.')
+                        print('1) ' + room.dropped[0])
                         take = input('\nDo you pick it up?\n1) Yes\n2) No\n')
                         if take == '1':
                             print(room.dropped[0] + ' added to inventory.')
@@ -195,12 +178,15 @@ def room_1():
                         print(room.dropped)
                         pickup = input('\nDo you pick them up?\n1) Yes\n2) No\n')
                         if pickup == '1':
-                            player1.add_inventory(room.dropped)
-                            print('Items added')        #BROKEN BROKEN BROKEN
-                            room.picked_up_multiple(room.dropped)
+                            for individualitems in room.dropped:
+                                index = room.dropped.index(individualitems)
+                                player1.add_inventory(room.dropped[index])
+                                room.picked_up(room.dropped[index])
+
 
 # Open inventory prompt
         elif userChoice == '2':
+            print('')
             player1.check_inventory()
 
 # Go to the table
@@ -208,16 +194,16 @@ def room_1():
         if userChoice == '3':
             table.show_table()
             if table.check_table() == 1:
-                take = input('Do you take the Key and the Red potion? \n1) Yes\n2) No\n')
+                take = input('\nDo you take the Key and the Red potion? \n1) Yes\n2) No\n')
                 print('')
                 if take == '1':
-                    print('Items Added')
+                    print('\nItems Added')
                     player1.add_inventory("Brass key")
                     player1.add_inventory("Red Potion")
                     table.take_key()
                     table.take_pot()
                 else:
-                    print('You leave them on the table')
+                    print('\nYou leave them on the table')
 
 
 room = Room()
@@ -225,7 +211,4 @@ player1 = Player()
 game_start()
 room_1()
 
-# my_function
-# myVar
-# MYCOMST
 
